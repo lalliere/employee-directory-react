cea65c59862ae887e3bfcb54fb5c70473a1f4b03
lalliere <emilylallier@yahoo.com>
2020-05-12 13:39:55 -0400

    init commit of title component

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Title.css";
+
+function Title() {
+  return (
+    <div>
+      <h1>University Employee Directory</h1>
+      <h2>Search for Staff Member</h2>
+    </div>
+  )
+}
+
+export default Title;
