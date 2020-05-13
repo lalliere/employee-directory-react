ff99b10685765c9cf101c425332edb9a8232586a
lalliere <emilylallier@yahoo.com>
2020-05-12 22:14:33 -0400

    added sort buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-//import Data from "./Components/Data"
-import EmployeeTable from "./components/EmployeeList";
+import EmployeeTable from "./components/EmployeeTable";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchBar from "./components/SearchBar";
 import data from "./components/Data.json";
@@ -10,21 +9,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: data
-    }
-    this.sortBy = this.sortBy.bind(this)
+      data: data,
+      search: ""
+    };
+    this.sortBy = this.sortBy.bind(this);
   }
+  // sortBy(key) {
+  //   this.setState({
+  //     data: data.sort((a, b) => a > b),
+  //   });
+  // }
+
+  compareBy(key) {
+    return function (a, b) {
+      if (a[key] < b[key]) return -1;
+      if (a[key] > b[key]) return 1;
+      return 0;
+    };
+  }
+
   sortBy(key) {
-    this.setState({
-      data: data.sort( (a,b) => a[key] > b[key])
-    })
+    let arrayCopy = [...this.state.data];
+    arrayCopy.sort(this.compareBy(key));
+    this.setState({data: arrayCopy});
   }
+
+
   render() {
     return (
       <div>
         <Title />
         <SearchBar />
-        <EmployeeTable data={this.state.data} />;
+        <EmployeeTable data={this.state.data} sortBy={this.sortBy}  />;
       </div>
     );
   }
---
+++ src/components/EmployeeTable.js
@@ -0,0 +1,52 @@
+import React from "react";
+//import Table from "react-bootstrap/Table";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Table } from "react-bootstrap";
+import "../styles/EmployeeTable.css";
+
+function EmployeeTable(props) {
+  return (
+    <Table striped bordered hover small size="sm">
+      <thead>
+        <tr>
+          <th>
+            <button onClick={() => props.sortBy("First_Name")}>
+              First Name
+            </button>
+          </th>
+          <th>
+            <button onClick={() => props.sortBy("Last_Name")}>
+              {" "}
+              Last Name
+            </button>
+          </th>
+          <th>
+            <button onClick={() => props.sortBy("Phone_number")}>
+              Phone Number{" "}
+            </button>
+          </th>
+          <th>
+            <button onClick={() => props.sortBy("Email")}> Email</button>
+          </th>
+          <th>
+            <button onClick={() => props.sortBy("Company_Name")}>
+              Company Name
+            </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((row) => (
+          <tr>
+            <td>{row.First_Name}</td>
+            <td>{row.Last_Name}</td>
+            <td>{row.Phone_number}</td>
+            <td>{row.Email}</td>
+            <td>{row.Company_Name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+export default EmployeeTable;
