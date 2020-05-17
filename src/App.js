8962c3504bf2919eaacd6382fbac397f6df2c9cd
lalliere <emilylallier@yahoo.com>
2020-05-17 10:52:12 -0400

    filter works AND Sort works

--- src/App.js
+++ src/App.js
@@ -1,46 +1,90 @@
 import React from "react";
 import EmployeeTable from "./components/EmployeeTable";
 import "bootstrap/dist/css/bootstrap.min.css";
-import SearchBar from "./components/SearchBar";
 import data from "./components/Data.json";
 import Title from "./components/Title";
+import "./styles/App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: data,
-      search: ""
+      searchFirst: "",
+      filteredTable: [],
+      sortDirection: "asc",
     };
     this.sortBy = this.sortBy.bind(this);
-  }
-  // sortBy(key) {
-  //   this.setState({
-  //     data: data.sort((a, b) => a > b),
-  //   });
-  // }
-
-  compareBy(key) {
-    return function (a, b) {
-      if (a[key] < b[key]) return -1;
-      if (a[key] > b[key]) return 1;
-      return 0;
-    };
   }
 
   sortBy(key) {
     let arrayCopy = [...this.state.data];
-    arrayCopy.sort(this.compareBy(key));
-    this.setState({data: arrayCopy});
+    arrayCopy.sort((a, b) => {
+      if (this.state.sortDirection === "asc") {
+        this.setState({ sortDirection: "desc" });
+        return a[key].toLowerCase() < b[key].toLowerCase() ? -1 : 1;
+      } else {
+        this.setState({ sortDirection: "asc" });
+        return a[key].toLowerCase() < b[key].toLowerCase() ? 1 : -1;
+      }
+    });
+    this.setState({ filteredTable: arrayCopy });
   }
 
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    }, this.filterTable);
+  };
+
+  filterTable() {
+    console.log(this.state.searchFirst)
+    // If theres no search or it is an empty string just return the unfiltered data
+    if (
+      this.state.searchFirst === null ||
+      this.state.searchFirst === "" ||
+      this.state.searchFirst === " "
+    ) {
+      this.setState({ filteredTable: data });
+    } else {
+      // otherwise sort the data and set this sorted data in state.
+      const items = this.state.data.filter(
+        (data) =>
+          data.First_Name.toLowerCase().includes(
+            this.state.searchFirst.toLowerCase()
+          ) ||
+          data.Last_Name.toLowerCase().includes(
+            this.state.searchFirst.toLowerCase()
+          ) ||
+          data.Phone_number.includes(this.state.searchFirst) ||
+          data.Email.toLowerCase().includes(this.state.searchFirst.toLowerCase()) ||
+          data.Deparment.toLowerCase().includes(
+            this.state.searchFirst.toLowerCase()
+          )
+      );
+      this.setState({ filteredTable: items });
+    }
+  }
+
+  componentDidMount() {
+    // Fire this function once the component is ready so we have data to send to the CoolTableComponent
+    this.filterTable();
+  }
 
   render() {
     return (
       <div>
         <Title />
-        <SearchBar />
-        <EmployeeTable data={this.state.data} sortBy={this.sortBy}  />
+        <form className="form">
+          <input className="search-bar"
+            value={this.state.searchFirst}
+            name="searchFirst"
+            onChange={this.handleInputChange}
+          />
+        </form>
+        <EmployeeTable data={this.state.filteredTable} sortBy={this.sortBy} />
       </div>
     );
   }
