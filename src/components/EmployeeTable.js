4f8507724bcd3a19704747192ebdd45df14e94e2
lalliere <emilylallier@yahoo.com>
2020-05-17 12:26:39 -0400

    added a div container for styling purposes

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -6,48 +6,50 @@ import "../styles/EmployeeTable.css";
 
 function EmployeeTable(props) {
   return (
-    <Table striped bordered hover small size="sm">
-      <thead>
-        <tr>
-          <th>
-            <button onClick={() => props.sortBy("First_Name")}>
-              First Name
-            </button>
-          </th>
-          <th>
-            <button onClick={() => props.sortBy("Last_Name")}>
-              Last Name
-            </button>
-          </th>
-          <th>
-            <button onClick={() => props.sortBy("Phone_number")}>
-              Phone Number
-            </button>
-          </th>
-          <th>
-            <button onClick={() => props.sortBy("Email")}> 
-            Email
-            </button>
-          </th>
-          <th>
-            <button onClick={() => props.sortBy("Department")}>
-              Department
-            </button>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.data.map((row) => (
-          <tr key={row.Email}>
-            <td>{row.First_Name}</td>
-            <td>{row.Last_Name}</td>
-            <td>{row.Phone_number}</td>
-            <td>{row.Email}</td>
-            <td>{row.Department}</td>
+    <div class="container">
+      <Table striped bordered hover small size="sm">
+        <thead>
+          <tr>
+            <th>
+              <button onClick={() => props.sortBy("First_Name")}>
+                First Name
+              </button>
+            </th>
+            <th>
+              <button onClick={() => props.sortBy("Last_Name")}>
+                Last Name
+              </button>
+            </th>
+            <th>
+              <button onClick={() => props.sortBy("Phone_number")}>
+                Phone Number
+              </button>
+            </th>
+            <th>
+              <button onClick={() => props.sortBy("Email")}> 
+              Email
+              </button>
+            </th>
+            <th>
+              <button onClick={() => props.sortBy("Department")}>
+                Department
+              </button>
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {props.data.map((row) => (
+            <tr key={row.Email}>
+              <td>{row.First_Name}</td>
+              <td>{row.Last_Name}</td>
+              <td>{row.Phone_number}</td>
+              <td>{row.Email}</td>
+              <td>{row.Department}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 export default EmployeeTable;
